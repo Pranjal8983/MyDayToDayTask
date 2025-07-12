4f04d4dad0bf95fa37e89eab848def960ce9ccd2
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-13 01:29:12 +0530

    Update main.tf

--- usecase4/main.tf
+++ usecase4/main.tf
@@ -1,1 +1,11 @@
+module "ec2_devlock" {
+  source     = "./modules/ec2-devlock"
+  aws_region = var.aws_region
+  ami_id     = var.ami_id
+  key_name   = var.key_name
+}
+
+output "alb_dns" {
+  value = module.ec2_devlock.alb_dns
+}
 
